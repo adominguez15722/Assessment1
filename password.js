eabf4193613cd0c4278b55c76c121e9f672d2fcd
Anthony <adominguez15722@gmail.com>
2022-01-07 11:31:12 -0600

    1 additional check on password

--- password.js
+++ password.js
@@ -5,19 +5,23 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
+let mySecret = 'Password123';
 
 reader.question("Welcome to the password validator tool! What is your password?", function(input){
 	tokens = input.split(' ');
 	
 	password = tokens[0];
 	
-    if (password.length >= 10)  {
+    if (password === mySecret){
+        console.log('Congratulations!! You guessed my password!')
+
+    }   else if (password.length >= 10)  {
         console.log('Success. Password is long enough!')
-    }   else{
+   
+    }   else {
         console.log('Failure. Password is not long enough.')
     };
-
+        
 
 	
 	
