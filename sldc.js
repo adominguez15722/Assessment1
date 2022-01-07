92a4b9e2f0430106dc865b129dd0193c2531c349
Anthony <adominguez15722@gmail.com>
2022-01-07 09:55:08 -0600

    Problem 2 is sovled

--- sldc.js
+++ sldc.js
@@ -26,3 +26,13 @@ Maintain
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This is the second step  where developers determine what is going to be required of their software. The client will usually be the one that provides an abstract idea of what they want as an end result."
+
+const design = "This is the third step where developers will put together a bird's eye view of the overall code and how it will all fit and work together. The language, OS, and hardware should all be known by the end of this step. A proof of concept should be the end of result of this step."
+
+const implementation = "This is the fourth step where software developers will program the code for the project."
+
+const testingAndIntegration = "This is the fifth step where developers ensure that any bugs and defects are recognized as soon as possible in order to fix them before the code goes live. After bugs are fixed, the code will go live."
+
+const maintain = "This is the final and ongoing step with SDLC. This is where maintaining and enhancing software to cope with newly discovered faults or requirements is done. This process will be ongoing until new software is implemented, thus starting the SDLC over again."
